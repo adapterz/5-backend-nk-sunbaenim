4c9f5216b806fb5d460126ead85f8262419440ca
nknkcho <srirachacho@gmail.com>
2022-04-05 17:30:43 +0900

    index.js : 회원가입(signup) router 내 controller name 변경

--- sunbaenim/api/users/index.js
+++ sunbaenim/api/users/index.js
@@ -3,21 +3,22 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("./users.ctrl");
 
+
 //회원가입
-router.post("/signup", ctrl.signup);
+router.post("/signup", ctrl.create_account);
 
 //내 닉네임 등록
-router.post("/:user_id/nickname", ctrl.get_nickname);
+router.post("/:user_id/nickname", ctrl.create_nickname);
 
 //TODO: 프로필 이미지 API
 //내 프로필 이미지 등록
-router.post("/:user_id/image", ctrl.get_image);
+router.post("/:user_id/image", ctrl.create_profile_image);
 
 //내 관심분야 등록
-router.post("/:user_id/fields", ctrl.get_field);
+router.post("/:user_id/fields", ctrl.create_field);
 
 //회원탈퇴
-router.delete("/signout", ctrl.signout);
+router.delete("/signout", ctrl.delete_account);
 
 //FIXME: 로그인, 로그아웃 API
 //TODO: 로그인, 로그아웃을 구현하기 위해 알아야 하는 개념 (session, async, await, mysql)
@@ -28,7 +29,7 @@ router.post("/login", ctrl.login);
 router.post("/logout", ctrl.logout);
 
 //비밀번호 찾기
-router.post("/pwd_search", ctrl.get_pwd);
+router.post("/pwd_search", ctrl.find_pwd);
 
 //내 닉네임 변경
 router.patch("/:user_id/nickname", ctrl.edit_nickname);
