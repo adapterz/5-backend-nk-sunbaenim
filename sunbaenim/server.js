f263497634001f2b92ad8f6c817409edf4cc6fb9
nknkcho <srirachacho@gmail.com>
2022-04-06 11:44:58 +0900

    server.js : added cors dependency

--- sunbaenim/server.js
+++ sunbaenim/server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 const users = require("./api/users/index");
 const articles = require("./api/articles/index");
@@ -9,8 +19,6 @@ const bookmarks = require("./api/bookmarks/index");
 const comments = require("./api/comments/index");
 const notifications = require("./api/notifications/index");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/users", users);
 app.use("/articles", articles);
