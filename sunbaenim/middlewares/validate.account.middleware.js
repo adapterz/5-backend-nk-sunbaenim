f8697359f3aa486ff68d9dd86dccfe117cf03795
nknkcho <srirachacho@gmail.com>
2022-04-15 18:41:23 +0900

    유저 회원가입 시 유효성 검사를 위한 middleware 추가 코드 설명을 위한 주석 추가
    :wq

--- sunbaenim/middlewares/validate.account.middleware.js
+++ sunbaenim/middlewares/validate.account.middleware.js
@@ -1,17 +1,20 @@
 const { body, validationResult } = require("express-validator");
 
+//에러코드 상수화
 const status_code = {
   invalid_input: 400,
 }
 
 module.exports = [
   body("email")
+  //email 유효성 검사
     .notEmpty()
     .withMessage("Please fill out email")
     .isEmail()
     .withMessage("Input as an email format")
     .bail(),
 
+  //비밀번호 유효성 검사
   body("pwd")
     .notEmpty()
     .withMessage("Please fill out pwd")
@@ -19,6 +22,7 @@ module.exports = [
     .withMessage("Input password more than 5 words"),
 
 
+  //비밀번호 일치 여부 확인
   body("pwd_check")
   .custom((value, { req }) => {
     if (value !== req.body.pwd) {
@@ -30,7 +34,7 @@ module.exports = [
 
   (req, res, next) => {
     const errors = validationResult(req);
-
+    //모든 유효성 검사 통과 시 next() 실행
     if(errors.isEmpty()) return next();
 
     if(!errors.isEmpty() && errors.errors[0].param === 'email') {
