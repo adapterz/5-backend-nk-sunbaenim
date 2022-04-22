eb3da1283b2117097914cde2410a1deb25cc3a26
nknkcho <srirachacho@gmail.com>
2022-04-22 13:58:20 +0900

    로그인, 로그아웃 관련 미들웨어, 컨트롤러 추가, 비밀번호 변경 관련 유효성 검사 미들웨어 추가,  기능 가시화를 위해 라우팅 순서 정리, 주석으로 세부 설명 추가

--- sunbaenim/routers/user.router.js
+++ sunbaenim/routers/user.router.js
@@ -5,11 +5,12 @@ const router = express.Router();
 const ctrl = require("../controllers/users.ctrl");
 //Middlewares
 const account_validation = require("../middlewares/validate.account.middleware");
+const pwd_validation = require("../middlewares/validate.pwd.middleware");
 const login_validation = require("../middlewares/login.middleware");
 const upload = require("../middlewares/multer.middleware");
 
 //이미 로그인 되어있는 유저인지 확인하는 미들웨어
-const logged_in = (req, res, next) => {
+const if_logged_in = (req, res, next) => {
   if(req.session.user_id){
     console.log("User already logged in!")
     return res.redirect("/");
@@ -18,23 +19,27 @@ const logged_in = (req, res, next) => {
 }
 
 //회원가입
-router.post("/signup", account_validation, ctrl.create_account);
+//router.get("/signup", if_logged_in, ctrl.create_account_page);
+router.post("/signup", if_logged_in, account_validation, ctrl.create_account);
 
 //내 닉네임 등록
+//닉네임 등록과 변경 시, 동일한 알고리즘이므로 같은 컨트롤러 사용
 router.post("/:user_id/nickname", ctrl.create_nickname);
+//내 닉네임 변경
+router.patch("/:user_id/nickname", ctrl.create_nickname);
 
 //내 프로필 이미지 등록
+//이미지 등록과 변경 시, 동일한 알고리즘이므로 같은 컨트롤러 사용
 router.post("/:user_id/image", upload.single('avatar'), ctrl.create_profile_image);
+//내 프로필 이미지 변경
+router.patch("/:user_id/nickname", upload.single('avatar'), ctrl.create_profile_image);
 
 //내 관심분야 등록
 router.post("/:user_id/fields", ctrl.create_field);
 
-//회원탈퇴
-router.delete("/signout", ctrl.delete_account);
-
 //로그인
 // router.get("/login", ctrl.login_page);
-router.post("/login", logged_in, login_validation, ctrl.login);
+router.post("/login", if_logged_in, login_validation, ctrl.login);
 
 //로그아웃
 router.get("/logout", (req, res, next) => {
@@ -47,13 +52,10 @@ router.get("/logout", (req, res, next) => {
 //비밀번호 찾기
 router.post("/pwd_search", ctrl.find_pwd);
 
-//내 닉네임 변경
-router.patch("/:user_id/nickname", ctrl.edit_nickname);
-
-//내 프로필 이미지 변경
-router.patch("/:user_id/image", ctrl.edit_image);
+//회원탈퇴
+router.patch("/:user_id/signout", ctrl.delete_account);
 
 //내 비밀번호 변경
-router.patch("/:user_id/pwd", ctrl.edit_pwd);
+router.patch("/:user_id/pwd", pwd_validation, ctrl.edit_pwd);
 
 module.exports = router;
