d18374b6b5d3932d61cafa3354ce6b8a50c616fd
nknkcho <srirachacho@gmail.com>
2022-04-22 13:53:47 +0900

    Promise 사용을 위해 mysql 모듈 변경, createPoot 도입

--- sunbaenim/config/mysql.js
+++ sunbaenim/config/mysql.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
-const mysql = require("mysql");
+const mysql = require("mysql2");
 require("dotenv").config();
 
-const mysqlConnect = mysql.createConnection({
+const mysqlConnect = mysql.createPool({
       host: process.env.host,
       user: process.env.user,
       password: process.env.password,
       database: process.env.database
 });
 
-module.exports = mysqlConnect;
+module.exports = mysqlConnect.promise();
