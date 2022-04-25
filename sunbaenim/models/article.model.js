e4268f24dd76b6013f2eb6b3ce09335c0660f06e
nknkcho <srirachacho@gmail.com>
2022-04-25 19:05:58 +0900

    게시판 관련 모델 추가, 페이지 조회기능 구현 중

---
+++ sunbaenim/models/article.model.js
@@ -0,0 +1,92 @@
+//db 연결
+const sql = require("../config/mysql");
+
+const Article = {
+  //게시글 생성
+  create: async function (
+    user_id,
+    title,
+    content,
+    views,
+    total_likes,
+    category_id,
+    is_published,
+    total_comments,
+    edit_at,
+    delete_at
+  ) {
+    //게시글 생성 일자
+    const create_at = new Date().toISOString().slice(0, 10).replace("T", " ");
+    sql.execute(
+      "INSERT INTO articles (user_id, title, content, views, total_likes, category, is_published, total_comments, create_at, edit_at, delete_at) VALUES (?,?,?,?,?,?,?,?,?,?,?)",
+      [
+        user_id,
+        title,
+        content,
+        views,
+        total_likes,
+        category_id,
+        is_published,
+        total_comments,
+        create_at,
+        edit_at,
+        delete_at,
+      ]
+    );
+  },
+
+  //게시글의 파일 추가
+  create_files: async function (user_id, file_name) {
+    //게시글의 파일 생성 일자
+    const create_at = new Date().toISOString().slice(0, 10).replace("T", " ");
+    sql.execute(
+      "INSERT INTO files (user_id, file_name, create_at) VALUES (?,?,?)",
+      [user_id, file_name, create_at]
+    );
+  },
+
+
+  //게시글 수정
+  edit: async function (article_id, title, content, category_id) {
+    //게시글의 수정 일자
+    const edit_at = new Date().toISOString().slice(0, 10).replace("T", " ");
+    sql.execute(
+      "UPDATE articles SET title = ?, content = ?, category = ?, edit_at = ? WHERE id = ?",
+      [title, content, category_id, edit_at, article_id]
+    );
+  },
+
+  //게시글 삭제
+  delete: async function (article_id) {
+    const is_published = 0;
+    const create_at = null;
+    const delete_at = new Date().toISOString().slice(0, 10).replace("T", " ");
+    sql.execute(
+      "UPDATE articles SET is_published = ?, create_at = ?, delete_at = ? WHERE id = ?",
+      [is_published, create_at, delete_at, article_id]
+    );
+  },
+
+  //게시글 식별 id로 게시글 정보 조회
+  find_by_id: async function (article_id) {
+    const [row] = await sql.execute("SELECT * FROM articles WHERE id = ?", [
+      article_id,
+    ]);
+    return row;
+  },
+
+  //게시글을 작성한 유저 id로 게시글 정보 조회
+  find_by_user_id: async function (user_id) {
+    const [row] = await sql.execute(
+      "SELECT * FROM articles WHERE user_id = ?",
+      [user_id]
+    );
+    return row;
+  },
+
+  //게시글 목록 조회
+  get_articles: async function(page, page_size) {}
+    
+};
+
+module.exports = Article;
