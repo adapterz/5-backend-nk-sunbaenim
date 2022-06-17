0c4db41ee5f4110b92820c0650a25bbe879c86ec
nknkcho <srirachacho@gmail.com>
2022-06-18 02:28:49 +0900

    get 요청에 따른 이미지 조회 컨트롤러 추가

--- sunbaenim/controllers/files.ctrl.js
+++ sunbaenim/controllers/files.ctrl.js
@@ -1,3 +1,4 @@
+const path = require("path");
 //Models 상수화
 const Models = require("../models/models");
 //상태코드 상수화
@@ -9,21 +10,21 @@ const logger = require('../config/winston');
 const create_files = async (req, res, next) => {
   try {
     const { user_id } = req.session;
+    const { article_id } = req.params;
     //게시글에 업로드된 파일들을 db에 저장
     await Promise.all(
-      req.files.map((v) => Models.File.create_files(user_id, v.filename))
+      req.files.map((v) => Models.File.create_files(user_id, article_id, v.filename))
     );
     //업로드가 성공적으로 반영되었으며, 반환해야 할 콘텐츠가 없으므로 상태코드 204
     return res.status(status.NO_CONTENT).end();
-  } catch (err) {
+  } catch (error) {
     logger.error(`file: files.ctrl.js, location: create_files(), error: ${err}`);
-    next(err);
+    next(error);
   }
 };
 
-
-// POST: "/:user_id/nickname" 유저의 프로필 이미지 등록 요청 시 응답 (이미지 등록)
-// PATCH: "/:user_id/nickname" 유저의 프로필 이미지 변경 요청 시 응답 (이미지 등록)
+// POST: "/:user_id" 유저의 프로필 이미지 등록 요청 시 응답 (이미지 등록)
+// PATCH: "/:user_id" 유저의 프로필 이미지 변경 요청 시 응답 (이미지 등록)
 const create_profile_image = async (req, res, next) => {
   try {
     const { filename } = req.file;
@@ -49,14 +50,61 @@ const create_profile_image = async (req, res, next) => {
     );
     //요청은 성공적으로 반영되었으나, 응답으로 반환해줄 콘텐츠는 없는 경우 상태코드 204 반환
     return res.status(status.NO_CONTENT).end();
-  } catch(err) {
+  } catch(error) {
     logger.error(`file: files.ctrl.js, location: create_profile_image(), error: ${err}`);
-    next(err);
+    next(error);
   }
 };
+
+const get_profile_image = async (req, res, next) => {
+  try{
+    const { user_id } = req.session;
+
+    const file_name = await Models.File.get_file_by_user_id(user_id);
+    logger.info(
+      `file: files.ctrl.js, location: Models.File.get_file_by_user_id(${user_id}), msg: Get user profile img`
+    );
+    if(file_name.length == 0){
+      logger.info(
+        `file: files.ctrl.js, location: Models.File.get_file_by_user_id(${user_id}), msg: NO user profile img`
+      )
+      return res.status(status.NOT_FOUND).end();
+    }
+    const file = file_name[0].file_name;
+    return res.status(status.OK).sendFile(path.resolve('./public/images/' + file));
+  } catch(error) {
+    logger.error(`file: files.ctrl.js, location: get_profile_image(), error: ${err}`);
+    next(error);
+  }
+}
+
+const get_writer_profile_image = async (req, res, next) => {
+  try{
+    const { user_id } = req.params;
+
+    const file_name = await Models.File.get_file_by_user_id(user_id);
+    if(file_name.length == 0){
+      logger.info(
+        `file: files.ctrl.js, location: Models.File.get_file_by_user_id(${user_id}), msg: NO user profile img`
+      )
+      return res.status(status.NOT_FOUND).end();
+    }
+
+    logger.info(
+      `file: files.ctrl.js, location: Models.File.get_file_by_user_id(${user_id}), msg: Get user profile img`
+    );
+    const file = file_name[0].file_name;
+    return res.status(status.OK).sendFile(path.resolve('./public/images/' + file));
+  } catch(error) {
+    logger.error(`file: files.ctrl.js, location: get_profile_image(), error: ${err}`);
+    next(error);
+  }
+}
 
 
 module.exports = {
   create_files,
-  create_profile_image
+  create_profile_image,
+  get_profile_image,
+  get_writer_profile_image
 }
