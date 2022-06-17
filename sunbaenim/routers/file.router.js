b6612e8870bc47a1d32fcaa99bda7eb7db553ceb
nknkcho <srirachacho@gmail.com>
2022-06-18 02:44:41 +0900

    프론트엔드 요청에 따른 필요 라우터 추가

--- sunbaenim/routers/file.router.js
+++ sunbaenim/routers/file.router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 //Controllers
 const ctrl = require("../controllers/files.ctrl");
@@ -9,11 +10,18 @@ const upload = require("../middlewares/multer.middleware");
 //로그인 되어있는 유저인지 확인하는 미들웨어
 const login_check = require("../middlewares/logincheck.middleware");
 
+
 //게시글 내 파일 생성
-router.post("/", login_check.if_not_logged_in, upload.array('article_files'), ctrl.create_files);
+router.post("/:article_id", login_check.if_not_logged_in, upload.array('article_files'), ctrl.create_files);
 
 //내 프로필 이미지 등록
 //이미지 등록과 변경 시, 동일한 알고리즘이므로 같은 api 사용
-router.post("/:user_id", login_check.if_logged_in, upload.single('avatar'), ctrl.create_profile_image);
+router.post("/user/:user_id", login_check.if_logged_in, upload.single('avatar'), ctrl.create_profile_image);
+
+//내 프로필 이미지 받기
+router.get("/user", login_check.if_not_logged_in, ctrl.get_profile_image);
+
+//특정 유저 프로필 이미지 받기
+router.get("/user/:user_id", ctrl.get_writer_profile_image);
 
 module.exports = router;
